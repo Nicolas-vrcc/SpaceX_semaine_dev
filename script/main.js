707af1c2727216ea124fe4d0d665b57f13cdea0c
nicolas <nicolas.vrillac@hetic.net>
2017-12-18 17:37:35 +0100

    minor advancement
    slider implementation

--- script/main.js
+++ script/main.js
@@ -1,12 +1,24 @@
 // get DOM elements
 const homePage = document.querySelector('.home')
 const startBtn = homePage.querySelector('.startbtn')
-const mainSlider = document.querySelector('.slider');
+const mainSlider = document.querySelector('.slider')
+const slide1 = document.querySelector('.slide1')
+const slide2 = document.querySelector('.slide2')
 
 // event listeners
 startBtn.addEventListener('click', (e) =>{
   homePage.style.opacity = 0
+  // wait for transition end before setting display none
+  homePage.addEventListener("transitionend", function(event) {
+  homePage.style.display = 'none'
+})
 })
 mainSlider.addEventListener('click', (e) =>{
   console.log('click');
+})
+
+//handle the animation of the slides
+window.addEventListener('keypress', (e) =>{
+  console.log(slide1);
+    slide2.classList.toggle("animate")
 })
